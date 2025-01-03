2500cd48aa69a1f6a8d4ae85d1fad6d0b968df7e
john mckie <john.mckie@remarkable.net>
2025-01-03 16:16:29 +0000

    Started to add in the DOM update work

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.
 const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?api_key=120737b6c47965cfd26124583f33c7d3&query="'
 
+const main = document.getElementById('main');
 const form = document.getElementById('form');
 const search = document.getElementById('search');
 
@@ -12,8 +13,18 @@ async function getMovies(url) {
 	const res = await fetch(url);
 	const data = await res.json();
 
-	console.log(data.results);
+	showMovies(data.results);
 	
+}
+
+function showMovies(movies) {
+	main.innerHTML = '';
+
+	movies.forEach(movie => {
+		const { title, poster_path, vote_average, overview } = movie;
+		
+	})
+
 }
 
 
