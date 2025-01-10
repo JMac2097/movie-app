e1519755dddf60b8b6dbe7d3ccd78a7f4786d300
john mckie <john.mckie@remarkable.net>
2025-01-10 17:17:09 +0000

    reset functionality added in

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
 const API_URL =
-  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=120737b6c47965cfd26124583f33c7d3&page=1";
-const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+  'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=120737b6c47965cfd26124583f33c7d3&page=1';
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280';
 const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=120737b6c47965cfd26124583f33c7d3&query="';
 
-const main = document.getElementById("main");
-const form = document.getElementById("form");
-const search = document.getElementById("search");
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+const resetButton = document.querySelector('.js-reset-search-button');
 
 getMovies(API_URL);
 
@@ -18,13 +19,13 @@ async function getMovies(url) {
 }
 
 function showMovies(movies) {
-  main.innerHTML = "";
+  main.innerHTML = '';
 
   movies.forEach((movie) => {
     const { title, poster_path, vote_average, overview } = movie;
 
-    const movieEl = document.createElement("div");
-    movieEl.classList.add("movie");
+    const movieEl = document.createElement('div');
+    movieEl.classList.add('movie');
 
     movieEl.innerHTML = `
 		<img
@@ -46,24 +47,37 @@ function showMovies(movies) {
 
 function getClassByRating(vote) {
   if (vote >= 8) {
-    return "green";
+    return 'green';
   } else if (vote >= 5) {
-    return "orange";
+    return 'orange';
   } else {
-    return "red";
+    return 'red';
   }
 }
 
-form.addEventListener("submit", (e) => {
+form.addEventListener('submit', (e) => {
   e.preventDefault();
-
   const searchTerm = search.value;
 
-  if (searchTerm && searchTerm !== "") {
+  if (searchTerm && searchTerm !== '') {
     getMovies(`${SEARCH_API}${searchTerm}"`);
 
-    search.value = "";
+    search.value = '';
+    toggleResetButton(false);
   } else {
     window.location.reload();
   }
 });
+
+function toggleResetButton(shouldHide) {
+  resetButton.classList.toggle('hidden', shouldHide);
+  if (!shouldHide) {
+    resetButton.addEventListener('click', resetFilms);
+  } else {
+    resetButton.removeEventListener('click', resetFilms);
+  }
+}
+
+function resetFilms() {
+  window.location.reload();
+}
